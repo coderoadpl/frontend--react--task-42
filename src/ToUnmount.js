2682846e3e98e0a06d70de28be83c578d948ea16
Mateusz Choma <kontakt@coderoad.pl>
2021-07-08 11:35:38 +0200

    `componentWillUnmount` added

--- src/ToUnmount.js
+++ src/ToUnmount.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 class ToUnmount extends React.Component {
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
+  }
+
   render () {
     return (
       <div>
