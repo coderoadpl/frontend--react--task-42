f1a30e595dd69cbc798ea8b99533436fe67d9987
Mateusz Choma <kontakt@coderoad.pl>
2021-07-08 11:41:03 +0200

    Clering interval in `componentWillUnmount`

--- src/ToUnmount.js
+++ src/ToUnmount.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
 class ToUnmount extends React.Component {
+  intervalId = null
+
+  componentDidMount () {
+    console.log('componentDidMount')
+
+    this.intervalId = setInterval(() => {
+      console.log('interval')
+      this.forceUpdate()
+    }, 1000)
+  }
+
   componentWillUnmount () {
     console.log('componentWillUnmount')
+
+    // this it a good place to clear effects
+    clearInterval(this.intervalId)
   }
 
   render () {
     return (
       <div>
-        ToUnmount
+        {(new Date()).toString()}
       </div>
     )
   }
